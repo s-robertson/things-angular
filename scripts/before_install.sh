ca41ae483d5c3df4a342a98b7e2696db91f96232
srobertson <srobertson@acromediainc.com>
2016-07-17 14:54:26 -0700

    Disable strict host checking in travis install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p ~\/.ssh
 openssl aes-256-cbc -K $encrypted_4ad8f554306a_key -iv $encrypted_4ad8f554306a_iv -in things-deploy.enc -out ~\/.ssh/things-deploy -d
+echo -e "Host robotscostrent.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
