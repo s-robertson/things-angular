e4eca986a9e0273d1d64a242f3f859f20749a2e2
srobertson <srobertson@acromediainc.com>
2016-07-17 17:37:31 -0700

    Add chrome to travis setup for tests

--- .travis/scripts/before_install.sh
+++ .travis/scripts/before_install.sh
@@ -2,3 +2,10 @@
 mkdir -p ~\/.ssh
 
 openssl aes-256-cbc -K $encrypted_14c735dec5a9_key -iv $encrypted_14c735dec5a9_iv -in .travis/things-deploy.enc -out ~\/.ssh/things-deploy -d
+
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+echo "========================"
+which google-chrome
+echo "========================"
