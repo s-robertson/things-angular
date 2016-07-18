1bfc94afcdfba2fd23b603d68d243a8bcc0a3614
srobertson <srobertson@acromediainc.com>
2016-07-17 17:31:42 -0700

    Change deploy script to only deploy the dist folder

--- .travis/scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-# Build the site with NPM
-npm run build:prod
--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -2,14 +2,18 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     eval "$(ssh-agent -s)"
+
+    cd dist
+    git init
+
     chmod 600 ~\/.ssh/things-deploy
     ssh-add ~\/.ssh/things-deploy
 
-    git fetch --unshallow
     git remote add deploy "things@robotscostrent.com:/var/www/things"
     git config user.name "Travis CI"
     git config user.email "srobertson203@gmail.com"
     git push --force deploy master
+
     echo "Deployed."
 else
     echo "Not deploying, since this branch isn't master."
