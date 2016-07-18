2382b6c681caba4a8bb9536ba6671c205cfbc763
srobertson <srobertson@acromediainc.com>
2016-07-17 19:40:34 -0700

    Add build command for Travis

--- .travis/scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p ~\/.ssh
-
-openssl aes-256-cbc -K $encrypted_14c735dec5a9_key -iv $encrypted_14c735dec5a9_iv -in .travis/things-deploy.enc -out ~\/.ssh/things-deploy -d
--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -3,18 +3,21 @@ set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     eval "$(ssh-agent -s)"
 
+    ls -al ~/.ssh
+    echo "=================================="
+
     cd dist
     git init
 
-    chmod 600 ~\/.ssh/things-deploy
-    ssh-add ~\/.ssh/things-deploy
+    chmod 600 ~/.ssh/things-deploy
+    ssh-add ~/.ssh/things-deploy
 
+    git add .
+    git commit -m "Deployment"
     git remote add deploy "things@robotscostrent.com:/var/www/things"
     git config user.name "Travis CI"
     git config user.email "srobertson203@gmail.com"
     git push --force deploy master
-
-    echo "Deployed."
 else
     echo "Not deploying, since this branch isn't master."
 fi
